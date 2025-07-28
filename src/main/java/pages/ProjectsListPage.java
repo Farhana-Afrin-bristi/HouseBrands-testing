1f9fd17f71e8e1eceff86d0f1b9550d1a567acc4
sabrina <coredeveloper.2013@gmail.com>
2025-07-28 12:06:43 +0600

    Project list page is added

--- src/main/java/pages/ProjectsListPage.java
+++ src/main/java/pages/ProjectsListPage.java
@@ -1,4 +1,20 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class ProjectsListPage {
+
+    private WebDriver driver;
+    private WebDriver wait;
+
+    public ProjectsListPage(WebDriver driver) {
+        this.driver = driver;
+
+    }
+
+
+
 }
