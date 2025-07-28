d37011a36d93c573549c569073118eba1708113e
sabrina <coredeveloper.2013@gmail.com>
2025-07-28 12:07:38 +0600

    Projects page is added

--- src/main/java/pages/ProjectsPage.java
+++ src/main/java/pages/ProjectsPage.java
@@ -1,4 +1,26 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class ProjectsPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public ProjectsPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public ProjectsListPage goToProjectsPage(){
+        WebElement projects = driver.findElement(By.xpath("//div[@data-i18n='Projects']"));
+        projects.click();
+
+        return new ProjectsListPage(driver);
+    }
 }
